477c9439ee421fa9db01b1ce938dd44651afc5b4
wcd19901010 <wangchengda@360fob.net>
2018-06-12 14:53:57 +0800

    添加部门微服务消费端测试代码

---
+++ microservicecloudconsumerdept80/src/main/java/com/itdoc/springcloud/DeptConsumerApp80.java
@@ -0,0 +1,28 @@
+/**
+ * @filename: DeptConsumerApp80.java
+ * @desc 部门微服务消费者启动类
+ * @author: Wang Chinda
+ * @blog http://www.cnblogs.com/goodcheap
+ * @date: 2018-06-12 13:53
+ * @version: v1.0
+ * @copyright: Copyright © 2018 ༄ྂ祸ྂྂ害ོ༘苍ྂྂ生ོ༘࿐ྂ 版权所有
+ * @modify_history: -
+ * 20180612   Wang Chinda   create
+ * 20180612   Wang Chinda   modify   method()
+ */
+package com.itdoc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @desc 部门微服务消费者启动类
+ * @author Wang Chinda
+ * @create 2018-06-12 13:53
+ */
+@SpringBootApplication
+public class DeptConsumerApp80 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumerApp80.class, args);
+    }
+}
---
+++ microservicecloudconsumerdept80/src/main/java/com/itdoc/springcloud/controller/DeptControllerConsumer.java
@@ -0,0 +1,68 @@
+/**
+ * @filename: DeptControllerConsumer.java
+ * @desc 部门微服务消费者控制层
+ * @author: Wang Chinda
+ * @blog http://www.cnblogs.com/goodcheap
+ * @date: 2018-06-12 14:03
+ * @version: v1.0
+ * @copyright: Copyright © 2018 ༄ྂ祸ྂྂ害ོ༘苍ྂྂ生ོ༘࿐ྂ 版权所有
+ * @modify_history: -
+ * 20180612   Wang Chinda   create
+ * 20180612   Wang Chinda   modify   method()
+ */
+package com.itdoc.springcloud.controller;
+
+import com.itdoc.springcloud.entities.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * @desc 部门微服务消费者控制层
+ * @author Wang Chinda
+ * @create 2018-06-12 14:03
+ */
+@RestController
+@RequestMapping("/consumer/dept/")
+public class DeptControllerConsumer {
+
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+
+    /**
+     * 使用restTemplate访问restful接口非常的简单粗暴无脑。 (url, requestMap,
+     * ResponseBean.class)这三个参数分别代表 REST请求地址、请求参数、HTTP响应转换被转换成的对象类型。
+     */
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * 添加部门信息
+     * @param dept
+     * @return
+     */
+    @PostMapping("add")
+    public boolean add(@RequestBody Dept dept) {
+        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
+    }
+
+    /**
+     * 根据id获取部门信息
+     * @param id
+     * @return
+     */
+    @GetMapping("get/{id}")
+    public Dept get(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
+    }
+
+    /**
+     * 获取全部部门信息
+     * @return
+     */
+    @GetMapping("list")
+    public List<Dept> list() {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+    }
+}
