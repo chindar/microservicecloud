0b82a2e1759cd5c38079c80d390b42055c0ae0eb
wang chinda <475450155@qq.com>
2018-06-16 17:36:18 +0800

    自定义Ribbon负载均衡算法

---
+++ microservicecloudproviderdept8003/src/main/java/com/itdoc/springcloud/DeptProviderApp8003.java
@@ -0,0 +1,34 @@
+/**
+ * @filename: DeptProviderApp8001.java
+ * @desc 8001服务启动类
+ * @author: Wang Chinda
+ * @blog http://www.cnblogs.com/goodcheap
+ * @date: 2018-06-12 11:30
+ * @version: v1.0
+ * @copyright: Copyright © 2018 ༄ྂ祸ྂྂ害ོ༘苍ྂྂ生ོ༘࿐ྂ 版权所有
+ * @modify_history: -
+ * 20180612   Wang Chinda   create
+ * 20180612   Wang Chinda   modify   method()
+ */
+package com.itdoc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @desc 8001服务启动类
+ * @author Wang Chinda
+ * @create 2018-06-12 11:30
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class DeptProviderApp8003 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProviderApp8003.class, args);
+    }
+
+}
